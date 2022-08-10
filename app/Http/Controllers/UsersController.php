0b2a0be10101ae0f1e8260f4cc8c241ecfc6c391
vagrant <vagrant@homestead>
2022-08-10 07:59:56 +0000

    upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,9 +19,21 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            } else {
+                // 上传有错误，withErrors可以携带回错误信息
+                return back()->withErrors(['上传图片格式只支持png, jpg, gif, jpeg这四种格式']);
+            }
+        }
+
+        //$user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功!');
     }
 }
