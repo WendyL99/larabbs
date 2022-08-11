11eb48fd554ee0ac73b519a23df0f5a5682b0395
vagrant <vagrant@homestead>
2022-08-11 05:37:32 +0000

    fix N+1 issue

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -47,3 +47,15 @@
 @else
     <div class="empty-block">暂无数据 ~_~</div>
 @endif
+
+
+@if(count($topics))
+  <ul>
+    @foreach($topics as $topic)
+      {{ $topic->user->name }}
+
+      {{ $topic->category->name }}
+      @endforeach
+  </ul>
+
+@endif
